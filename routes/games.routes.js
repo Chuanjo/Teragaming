3aa7c36789a38c0ea8d8acb4a9ca5d138f24ccba
Chuanjo <chuanjo@hotmail.com>
2022-02-25 11:37:09 +0100

    api rulando

--- routes/games.routes.js
+++ routes/games.routes.js
@@ -1,7 +1,20 @@
 const router = require("express").Router();
+const Rawger = require('rawger');
+const axios = require("axios")
 
 router.get("/", (req, res, next) => {
+  //console.log(process.env.GAMES_API_KEY)
+  axios.get(`https://api.rawg.io/api/games?key=${process.env.GAMES_API_KEY}`)
+  .then((response) => {
+    //console.log(response)
+    console.log(response.data.results.length)
+    console.log(response.data.results)
+  })
+
+
   res.render("index");
 });
+
+
 
 module.exports = router;
--- routes/index.routes.js
+++ routes/index.routes.js
@@ -1,4 +1,9 @@
 const router = require("express").Router();
+const { append } = require("express/lib/response");
+const gamesRoutes = require("./games.routes")
+
+router.use("/games", gamesRoutes)
+
 
 /* GET home page */
 router.get("/", (req, res, next) => {
