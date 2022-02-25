31fba8d6d992819930e813c7813941be72a6a7a4
juanma <fmjuan89@gmail.com>
2022-02-25 14:33:12 +0000

    lista done

--- routes/games.routes.js
+++ routes/games.routes.js
@@ -4,7 +4,7 @@ const axios = require("axios")
 router.get("/game-list", (req, res, next) => {
   axios.get(`https://api.rawg.io/api/games?key=${process.env.GAMES_API_KEY}`)
   .then((response) => {
-    console.log("Hola que tal",response.data.results[0])
+    console.log("Hola que tal",response.data.background_image[0])
     res.render("games/game-list", {data:response.data.results})
 
 
