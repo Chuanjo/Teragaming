8728fa811e070683d6bedb77b7c533899a6efd9f
juanma <fmjuan89@gmail.com>
2022-02-25 15:47:35 +0000

    somemore

--- routes/games.routes.js
+++ routes/games.routes.js
@@ -4,7 +4,7 @@ const axios = require("axios")
 router.get("/game-list", (req, res, next) => {
   axios.get(`https://api.rawg.io/api/games?key=${process.env.GAMES_API_KEY}`)
   .then((response) => {
-    console.log("Hola que tal",response.data.background_image[0])
+    // console.log("Hola que tal",response.data.background_image[0])
     res.render("games/game-list", {data:response.data.results})
 
 
@@ -14,6 +14,12 @@ router.get("/game-list", (req, res, next) => {
   })
 });
 
+router.get("/game-details/:id", (req, res, next)=>{
+  axios.get(`https://api.rawg.io/api/games?key=${process.env.GAMES_API_KEY}`)
+  .then((response)=>{
+    res.render("games/game-details", {data:response.data.results})
+  })
+})
 
 
 module.exports = router;
